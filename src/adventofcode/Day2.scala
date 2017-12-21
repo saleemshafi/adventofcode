70825365578a2d2d132a68eeb667dcd7cb52e77c
Saleem Shafi <mshafi@paypal.com>
2017-12-21 17:37:27 -0600

    Solve second half of Day 2
    The adjustment for the second half of this challenge was to use a
    different algorithm for calculating the checksum.
    
    I'm not particularly happy with the O(n^2) solution for the "evenly
    divisible" check, but with the size of the data we're dealing with, it's
    perfectly acceptable.

--- src/adventofcode/Day2.scala
+++ src/adventofcode/Day2.scala
@@ -7,24 +7,28 @@ package adventofcode
 
 object Day2 {
 
-  def diff(input: Array[Int]) = {
-    val nums = input.sorted
-    nums.last - nums.head
-  }
+  def minMaxDiff(nums: Array[Int]) = nums.max - nums.min
 
-  def checksum(row: String):Int =
-    diff(row.split("\\s")
-      .map(Integer.parseInt))
+  def evenDiv(nums: Array[Int]) =
+    (for (a <- nums;
+          b <- nums;
+          if (a < b && b % a == 0))
+      yield b / a).head
 
-  def checksum(rows: Array[String]):Int =
-    rows.map(checksum).sum
+  def calculate(algo: Array[Int]=>Int)(row: String):Int =
+    algo(row.split("\\s").map(Integer.parseInt))
+
+  def calculate(algo: Array[Int]=>Int, rows: Array[String]):Int =
+    rows.map(calculate(algo)).sum
 
   def main(args: Array[String]) = {
-    assert(checksum(Array("5 1 9 5", "7 5 3", "2 4 6 8")) == 18)
+    assert(calculate(minMaxDiff, Array("5 1 9 5", "7 5 3", "2 4 6 8")) == 18)
+    assert(calculate(evenDiv, Array("5 9 2 8", "9 4 7 3", "3 8 6 5")) == 9)
 
     val day2input = inputAsString("dat/day2.dat")
     var inputRows = day2input.split("\n")
 
-    println(checksum(inputRows)) // 36174
+    println(calculate(minMaxDiff, inputRows)) // 36174
+    println(calculate(evenDiv, inputRows)) //
   }
 }
