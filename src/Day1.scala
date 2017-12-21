175b68d9468cdc98ae66e88697d1dee6f3d00060
Saleem Shafi <mshafi@paypal.com>
2017-12-21 10:32:50 -0600

    Solve second half of day 1 problem
    The second half of the problem breaks the assumption that the character
    to compare is the next character in the string.  So, the checksum
    algorithm is modified to pick an arbitrary offset from the current
    character and uses zip instead of sliding to create the pairs that need
    to be compared.

--- src/Day1.scala
+++ src/Day1.scala
@@ -4,16 +4,18 @@
 
 
 object Day1 {
-  def checksum(input: String) = {
-    val full = input + input.charAt(0)
-    full.sliding(2)
-      .map( pair => (Integer.parseInt(pair.substring(0,1)), Integer.parseInt(pair.substring(1,2))))
-      .map( _ match {
+  def checksum(input: String, offset:Int = 1) = {
+    val analog = input.substring(offset) + input.substring(0, offset)
+    input.zip(analog)
+      .map( pair => (Integer.parseInt(pair._1.toString), Integer.parseInt(pair._2.toString)))
+      .map({
         case (a, b) if a == b => a
         case _ => 0
       })
       .sum
   }
+
+  def midChecksum(input: String) = checksum(input, input.length / 2)
 
   def inputAsString(filename: String) = {
     val source = scala.io.Source.fromFile(filename)
@@ -26,6 +28,14 @@ object Day1 {
     assert(checksum("1234") == 0)
     assert(checksum("91212129") == 9)
 
-    println(checksum(inputAsString("dat/day1.dat"))) // 1341
+    assert(midChecksum("1212") == 6)
+    assert(midChecksum("1221") == 0)
+    assert(midChecksum("123425") == 4)
+    assert(midChecksum("123123") == 12)
+    assert(midChecksum("12131415") == 4)
+
+    val day1input = inputAsString("dat/day1.dat")
+    println(checksum(day1input)) // 1341
+    println(midChecksum(day1input)) // 1348
   }
 }
