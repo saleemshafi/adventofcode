b9ad7a66ba88d2fcb9fb76312340247824dff1cb
Saleem Shafi <mshafi@paypal.com>
2017-12-22 12:17:59 -0600

    Solve second half of Day 3
    The change in the second half of Day 3 completely broke the initial
    strategy, which relied on the fact that each value could easily be
    located on the grid.  This was due to the fact that the numbers in each
    ring were in a distinct range.
    
    However, the new challenge breaks that and the values in each ring are
    all over the place and depend on a specific progression through the
    grid.
    
    The new solution forces a walk through the grid, and Scala Streams were
    used to setup the path without evaluating any more than necessary.

--- src/adventofcode/Day3.scala
+++ src/adventofcode/Day3.scala
@@ -1,45 +1,51 @@
 package adventofcode
 
+import scala.collection.mutable
+
 /**
   * Created by mshafi on 12/7/17.
   */
 object Day3 {
 
+  case class Coordinate(x:Int, y:Int) {
+    def manhattanDistance = Math.abs(x) + Math.abs(y)
+  }
 
-  def ringSize(value:Int, size:Int = 1):Int =
-      if (value <= size * size) {
-        size
-      } else {
-        ringSize(value, size+2)
-      }
+  def coordinates(x: Int, ys: Seq[Int]):Seq[Coordinate] = for (y <- ys) yield Coordinate(x,y)
+  def coordinates(xs: Seq[Int], y: Int):Seq[Coordinate] = for (x <- xs) yield Coordinate(x,y)
+  def coordinates(xs: Seq[Int], ys:Seq[Int]):Seq[Coordinate]= for (x <- xs; y <- ys) yield Coordinate(x,y)
 
-  def manhattanDistance(value:Int):Int = {
-    if (value == 1) {
-      return 0;
-    }
-    val size = ringSize(value)
-    val sideSize = size - 1
-    val ringNum =  sideSize / 2
+  def spiralGridPath(ring:Int = 0):Stream[Coordinate] = ring match {
+    case 0 => Coordinate(0,0) #:: spiralGridPath(ring+1)
+    case r => (
+        coordinates(r, -r+1 to r) ++ // right side
+        coordinates(-r to r-1 reverse, r) ++ // top side
+        coordinates(-r, -r+1 to r-1 reverse) ++ // left side
+        coordinates(-r to r, -r) // bottom side
+      ).toStream #::: spiralGridPath(r+1) // next ring
+  }
 
-    val bottomRight = Math.pow(size, 2).toInt
-    val bottomLeft = bottomRight - sideSize
-    val topLeft = bottomLeft - sideSize
-    val topRight = topLeft - sideSize
-    value match {
-      case bottom if (value >= bottomLeft) => Math.abs(value - bottomLeft - ringNum) + ringNum
-      case left if (value > topLeft) => ringNum + Math.abs(topLeft - value + ringNum)
-      case top if (value >= topRight) => Math.abs(topRight - value + ringNum) + ringNum
-      case right => ringNum + Math.abs(value - topRight + ringNum)
+  def adjacentSum() = {
+    def adjacentCoordinates(coord:Coordinate) = coordinates(coord.x-1 to coord.x+1, coord.y-1 to coord.y+1)
+
+    val valueMap = mutable.Map[Coordinate,Int]().withDefault(_ => 0)
+    (coord:Coordinate) => {
+      val value = adjacentCoordinates(coord).map(valueMap).sum
+      valueMap.put(coord, if (value == 0) 1 else value)
+      valueMap.get(coord).get
     }
   }
 
-
   def main(args: Array[String]) = {
-    assert(manhattanDistance(1) == 0)
-    assert(manhattanDistance(12) == 3)
-    assert(manhattanDistance(23) == 2)
-    assert(manhattanDistance(1024) == 31)
+    val spiralCoords = spiralGridPath()
 
-    println(manhattanDistance(289326)) // 419
+    assert(spiralCoords(1 - 1).manhattanDistance == 0)
+    assert(spiralCoords(12 - 1).manhattanDistance == 3)
+    assert(spiralCoords(23 - 1).manhattanDistance == 2)
+    assert(spiralCoords(1024 - 1).manhattanDistance == 31)
+
+    println(spiralCoords(289326 - 1).manhattanDistance)  // 419
+
+    println(spiralCoords.map(adjacentSum()).dropWhile( _ <= 289326).head) // 295229
   }
 }
