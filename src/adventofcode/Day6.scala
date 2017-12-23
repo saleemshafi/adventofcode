d56b9307dbd0e40f4758e8e677c6fe1e887f5dff
Saleem Shafi <mshafi@paypal.com>
2017-12-22 18:00:22 -0600

    Solve second half of Day 6
    The second half of this problem doesn't really require and new
    processing, but a little bit more information about the processing that
    is already being done.
    
    Instead of simply returning the index of the duplicate memory state,
    we need to return the index of both of the memory states that are
    duplicated, which we can easily do by keeping a Map instead of a Set.

--- src/adventofcode/Day6.scala
+++ src/adventofcode/Day6.scala
@@ -1,6 +1,6 @@
 package adventofcode
 
-import scala.collection.mutable.Set
+import scala.collection.mutable.Map
 
 /**
   * Created by mshafi on 12/9/17.
@@ -19,21 +19,29 @@ object Day6 {
 
   def maxBlock(memory: Array[Int]) = memory.indexOf(memory.max)
 
+  def key(memory: Array[Int]) = memory.mkString(",")
 
-  def findRepetition(memory:Array[Int]) = {
-    val seen:Set[String] = Set.empty
+  def findLoop(memory:Array[Int]):(Int, Int) = {
+    val seen:Map[String, Int] = Map.empty
     var iterations = 0
-    while (!seen.contains(memory.mkString(","))) {
-      seen.add(memory.mkString(","))
+    while (!seen.contains(key(memory))) {
+      seen.put(key(memory), iterations)
       distribute(maxBlock(memory), memory)
       iterations += 1
     }
-    iterations
+    (seen.get(key(memory)).get, iterations)
+  }
+
+  def loopSize(memory: Array[Int]) = {
+    val loop = findLoop(memory)
+    loop._2 - loop._1
   }
 
   def main(args:Array[String]) = {
-    assert(findRepetition(Array(0, 2, 7, 0)) == 5)
+    assert(findLoop(Array(0, 2, 7, 0))._2 == 5)
+    assert(loopSize(Array(0, 2, 7, 0)) == 4)
 
-    println(findRepetition(Array(4,	1,	15,	12,	0,	9,	9,	5,	5,	8,	7,	3,	14,	5,	12,	3)))  // 6681
+    println(findLoop(Array(4,	1,	15,	12,	0,	9,	9,	5,	5,	8,	7,	3,	14,	5,	12,	3))._2)  // 6681
+    println(loopSize(Array(4,	1,	15,	12,	0,	9,	9,	5,	5,	8,	7,	3,	14,	5,	12,	3)))  // 2392
   }
 }
