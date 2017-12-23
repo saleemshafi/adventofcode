79f06d984b23f5b573774ec64e24b4a3c91ef200
Saleem Shafi <mshafi@paypal.com>
2017-12-23 07:52:13 -0600

    Solve second half of Day 7
    The first half of Day 7 ignored the weights entirely and we simply
    needed to find the root node, which was easy enough because it's the
    only one that isn't the child of another node.
    
    The second half requires you to actually build the tree and calculate
    weights, so the solution build on the initial one but adds completely
    new functionality.
    
    I'm not particularly happy with it, but i've spent too much time on it,
    so we're going to move on.

--- src/adventofcode/Day7.scala
+++ src/adventofcode/Day7.scala
@@ -1,33 +1,75 @@
 package adventofcode
 
+import scala.collection.mutable.Map
+
 /**
   * Created by mshafi on 12/9/17.
   */
 object Day7 {
 
-  def processStructure(line:String):(String, Set[String]) = {
-    val name = line.substring(0, line.indexOf(" "))  // ignore weight
-    var childNodes:Set[String] = Set.empty
-    val childrenStart = line.indexOf("->")
-    if (childrenStart != -1) {
-      val children = line.substring(childrenStart + 3)
-      childNodes = children.split(", ").toSet
+  class Node(val tree:Map[String, Node], val id:String, val weight:Int, val children:Set[String] = Set.empty) {
+    var totalWeight = -1
+
+    def getTotalWeight:Int = {
+      if (totalWeight == -1) {
+        val childrensWeight = getChildren.toList.map(_.getTotalWeight).sum
+        totalWeight = weight + childrensWeight
+      }
+      totalWeight
     }
-    (name, childNodes)
+
+    def getChildren = children.map(tree)
+
+    tree.put(id, this)
+  }
+
+  object Node {
+    val InputFormat = """^(\w+) \((\d+)\)(?: -> (.+))?$""".r
+
+    def apply(tree:Map[String, Node])(info:String):Node = info match {
+      case InputFormat(id, weight, children) if children == null => new Node(tree, id, weight.toInt)
+      case InputFormat(id, weight, children) =>
+        new Node(tree, id, weight.toInt, children.split(", ").toSet)
+    }
+  }
+
+  def findRoot(nodes:Seq[Node]):String = {
+    var nodeSet:Set[String] = Set.empty
+    var childNodes:Set[String] = Set.empty
+
+    nodes.foreach(node => {
+      nodeSet = nodeSet + node.id
+      childNodes = childNodes ++ node.children
+    })
+
+    (nodeSet -- childNodes).head
+  }
+
+  def findUnbalanced(root:Node):Option[(Node, Int)] = {
+    val weightMap = root.getChildren.groupBy(_.getTotalWeight)
+    if (weightMap.keySet.size == 0) {
+      None
+    } else {
+      val normal = weightMap.find(entry => entry._2.size > 1).get._1
+      weightMap.find(entry => entry._2.size == 1) match {
+        case Some((outlier, _)) => {
+          val thisNode = weightMap(outlier).head
+          findUnbalanced(thisNode).orElse(Some(thisNode, normal - outlier))
+        }
+        case None => None
+      }
+    }
   }
 
   def main(args:Array[String]) = {
-    val input = inputAsListOfStrings("dat/day7.dat")
+    val tree:Map[String, Node] = Map.empty
+    val nodes = inputAsListOfStrings("dat/day7.dat").map(Node.apply(tree))
+    val root = tree(findRoot(nodes))
 
-    var nodes:Set[String] = Set.empty
-    var childNodes:Set[String] = Set.empty
+    println(root.id)   // bpvhwhh
 
-    input.map(processStructure)
-      .foreach(struct => {
-        nodes = nodes + struct._1
-        childNodes = childNodes ++ struct._2
-      })
-
-    println((nodes -- childNodes).head)   // bpvhwhh
+    findUnbalanced(root).foreach( {
+      case (unbalanced, diff) => println(unbalanced.id+" is off by "+diff+".  It should weigh "+(unbalanced.weight+diff))
+    })   // tulwp is off by -8.  It should weigh 256
   }
 }
