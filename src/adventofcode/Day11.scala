0c8a31739017b1414dbaa57cf2bd6fa2568439fe
Saleem Shafi <mshafi@paypal.com>
2017-12-23 17:31:01 -0600

    Solve first half of Day 11

---
+++ src/adventofcode/Day11.scala
@@ -0,0 +1,84 @@
+package adventofcode
+
+/**
+  * Created by mshafi on 12/14/17.
+  */
+object Day11 {
+
+  class OptimizedPath {
+    var Ns, Ss, NEs, NWs, SEs, SWs = 0
+
+    def numSteps = Ns + Ss + NEs + NWs + SEs + SWs
+
+    def takeStep(step:String) = {
+      step match {
+        case "n" => goNorth
+        case "s" => goSouth
+        case "ne" => goNorthEast
+        case "nw" => goNorthWest
+        case "se" => goSouthEast
+        case "sw" => goSouthWest
+      }
+    }
+
+    def goNorth = {
+      if (Ss > 0) { Ss -= 1 }
+      else if (SEs > 0) { SEs -= 1; NEs += 1; }
+      else if (SWs > 0) { SWs -= 1; NWs += 1; }
+      else { Ns += 1 }
+    }
+
+    def goSouth = {
+      if (Ns > 0) { Ns -= 1 }
+      else if (NEs > 0) { NEs -= 1; SEs += 1; }
+      else if (NWs > 0) { NWs -= 1; SWs += 1; }
+      else { Ss += 1 }
+    }
+
+    def goNorthWest = {
+      if (SEs > 0) { SEs -= 1 }
+      else if (Ss > 0) { Ss -= 1; SWs += 1; }
+      else if (NEs > 0) { NEs -= 1; Ns += 1; }
+      else { NWs += 1 }
+    }
+
+    def goNorthEast = {
+      if (SWs > 0) { SWs -= 1 }
+      else if (Ss > 0) { Ss -= 1; SEs += 1; }
+      else if (NWs > 0) { NWs -= 1; Ns += 1; }
+      else { NEs += 1 }
+    }
+
+    def goSouthWest = {
+      if (NEs > 0) { NEs -= 1 }
+      else if (Ns > 0) { Ns -= 1; NWs += 1; }
+      else if (SEs > 0) { SEs -= 1; Ss += 1; }
+      else { SWs += 1 }
+    }
+
+    def goSouthEast = {
+      if (NWs > 0) { NWs -= 1 }
+      else if (Ns > 0) { Ns -= 1; NEs += 1; }
+      else if (SWs > 0) { SWs -= 1; Ss += 1; }
+      else { SEs += 1 }
+    }
+
+  }
+
+  def numSteps(data:String) = {
+    val steps = data.split(",").map(_.trim)
+    val path = new OptimizedPath
+    steps.foreach(path.takeStep)
+    path.numSteps
+  }
+
+  def main(arg:Array[String]): Unit = {
+    val data = inputAsString("dat/day11.dat")
+
+    assert(numSteps("ne,ne,ne") == 3)
+    assert(numSteps("ne,ne,sw,sw") == 0)
+    assert(numSteps("ne,ne,s,s") == 2)
+    assert(numSteps("se,sw,se,sw,sw") == 3)
+    println(numSteps(data))
+  }
+}
