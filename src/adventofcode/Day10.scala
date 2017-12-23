97211b7538beac9a18c8bdd077e42749f0b62cf4
Saleem Shafi <mshafi@paypal.com>
2017-12-23 17:14:37 -0600

    Solve second half of Day 10
    The second half of this problem just expands on the previous with some
    extra functionality.  Nothing too interesting here.

--- src/adventofcode/Day10.scala
+++ src/adventofcode/Day10.scala
@@ -21,25 +21,57 @@ object Day10 {
   }
 
   def pinch(list:Array[Int], lengths:Array[Int]): Unit = {
+    pinch(0)(list, lengths)
+  }
+
+  def pinch(rounds:Int)(list:Array[Int], lengths:Seq[Int]): Unit = {
     var index = 0
-    for (i <- 0 to lengths.length-1) {
-      index = pinch(list, lengths(i), index) + i
+    var skip = 0
+    for (i <- 1 to rounds) {
+      for (len <- lengths) {
+        index = pinch(list, len, index) + skip
+        skip += 1
+      }
     }
   }
+
+  def lengths(key:String) = key.map(_.toInt) ++ Array(17, 31, 73, 47, 23)
+
+  def sparseHash(list:Array[Int], key:String): Array[Int] = {
+    pinch(64)(list, lengths(key))
+    list
+  }
+
+  def denseHash(list:Array[Int], key:String) =
+    for (set <- sparseHash(list, key).grouped(16))
+      yield set.reduceRight((a, b) => a ^ b)
+
+  def toHex(i:Int) = i.toHexString match {
+    case hex if hex.length == 1 => "0" + hex
+    case hex => hex
+  }
+
+  def knotHash(key:String): String =
+    denseHash((0 to 255).toArray, key).map(toHex).mkString("")
 
   def main(args:Array[String]): Unit = {
     val testList = (0 to 4).toArray
     val testLengths = Array(3, 4, 1, 5)
-    pinch(testList, testLengths)
+    pinch(1)(testList, testLengths)
     assert(testList(0) == 3)
     assert(testList(1) == 4)
 
+    assert(knotHash("") == "a2582a3a0e66e6e86e3812dcb672a272")
+    assert(knotHash("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd")
+    assert(knotHash("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d")
+    assert(knotHash("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e")
 
     val list = (0 to 255).toArray
-    val lengths = Array(106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36)
+    val key = Array(106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36)
 
-    pinch(list, lengths)
-
+    pinch(1)(list, key)
     println(list(0) * list(1))  // 11413
+
+    println(knotHash("106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36"))  // 7adfd64c2a03a4968cf708d1b7fd418d
   }
 }
