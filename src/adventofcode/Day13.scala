3a3a1176a10e038376dc54febc16b19376e00bd0
Saleem Shafi <mshafi@paypal.com>
2017-12-23 22:23:08 -0600

    Solve second half of Day 13
    The changes required for the second half of this challenge shouldn't
    actually have been that difficult.  However, there was one point of the
    requirements -- that there should be no captures rather than a score of
    0 -- that caused some consternation.

--- src/adventofcode/Day13.scala
+++ src/adventofcode/Day13.scala
@@ -6,27 +6,35 @@ package adventofcode
 object Day13 {
 
   case class Layer(depth:Int, range:Int) {
-    def value = depth * range
-    def mod = (range-1) * 2
-    def captures = depth % mod == 0
+    val value = depth * range
+    val mod = (range-1) * 2
+    def captures(delay:Int) =
+      (delay + depth) % mod == 0
   }
 
   def getLayers(input:Array[String]) =
     input.map(_.split(": ")).map(info => Layer(info(0).trim.toInt, info(1).trim.toInt))
 
-  def sumCaptured(layers:Array[Layer]) =
-    layers.filter(_.captures).map(_.value).sum
+  def sumCaptured(delay:Int = 0)(layers:Array[Layer]) =
+    layers.filter(_.captures(delay)).map(_.value).sum
+
+  def isCaptured(delay:Int = 0)(layers:Array[Layer]) =
+    layers.find(_.captures(delay)).isDefined
 
   def main(args:Array[String]): Unit = {
-
     val testLayers = getLayers(splitLines("""0: 3
                                   |1: 2
                                   |4: 4
                                   |6: 4
                                   |""".stripMargin))
-    assert(sumCaptured(testLayers) == 24)
+    assert(sumCaptured()(testLayers) == 24)
+    assert(sumCaptured(10)(testLayers) == 0)
+    assert(!isCaptured(10)(testLayers))
+
+    assert(Stream.from(0).filter(!isCaptured(_)(testLayers)).head == 10)
 
     val layers = getLayers(inputAsListOfStrings("dat/day13.dat"))
-    println(sumCaptured(layers))   // 1900
+    println(sumCaptured()(layers))   // 1900
+    println(Stream.from(0).filter(!isCaptured(_)(layers)).head) // 3966414
   }
 }
