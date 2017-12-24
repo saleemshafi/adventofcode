d3411c7d5873bf44f1d811794f17488a002640e5
Saleem Shafi <mshafi@paypal.com>
2017-12-24 07:20:36 -0600

    Solve second half of Day 14
    The second half of this problem built on the first, rather than
    requiring changes to it.  It's basically just a recursive fill algorithm
    to color connected regions of the grid.

--- src/adventofcode/Day14.scala
+++ src/adventofcode/Day14.scala
@@ -1,11 +1,14 @@
 package adventofcode
 
 import Day10.knotHash
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by mshafi on 12/23/17.
   */
 object Day14 {
+
+  type Grid = ArrayBuffer[ArrayBuffer[Char]]
 
   def fromHex(hex:Char) = Integer.parseInt(hex.toString, 16)
 
@@ -16,16 +19,42 @@ object Day14 {
   }
 
   def row(key:String) =
-    knotHash(key).map(fromHex).map(toBitmap).mkString("")
+    new ArrayBuffer[Char]() ++ knotHash(key).map(fromHex).map(toBitmap).mkString("")
 
   def grid(key:String) =
-    (0 to 127).map(key + "-" + _).map(row)
+    new ArrayBuffer ++ (0 to 127).map(key + "-" + _).map(row)
 
-  def numUsedSpaces(g:Seq[String]) = g.map(_.count(_ == '#')).sum
+  def numUsedSpaces(g:Grid) = g.map(_.count(_ != '.')).sum
+
+  def markGroups(grid:Grid):Int = {
+    var groupNum = 0
+
+    def markGroup(i: Int, j: Int): Unit =
+      if (i >= 0 && i < grid.size && j >= 0 && j < grid(i).size && grid(i)(j) == '#') {
+        grid(i)(j) = groupNum.toString.last
+        markGroup(i-1, j)
+        markGroup(i+1, j)
+        markGroup(i, j-1)
+        markGroup(i, j+1)
+      }
+
+    for (i <- grid.indices;
+         j <- grid(i).indices if grid(i)(j) == '#') {
+          groupNum += 1
+          markGroup(i, j)
+    }
+    groupNum
+  }
 
   def main(args:Array[String]) = {
-    assert(numUsedSpaces(grid("flqrgnkx")) == 8108)
+    val testGrid = grid("flqrgnkx")
 
-    println(numUsedSpaces(grid("ljoxqyyw")))
+    assert(numUsedSpaces(testGrid) == 8108)
+    assert(markGroups(testGrid) == 1242)
+
+    val realGrid = grid("ljoxqyyw")
+    println(numUsedSpaces(realGrid)) // 8316
+    println(markGroups(realGrid))  // 1074
+
   }
 }
