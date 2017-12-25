da9f271ff14ec75018aa4ead523bd26a7673183b
Saleem Shafi <mshafi@paypal.com>
2017-12-24 21:29:35 -0600

    Solve second half of Day 17
    The second half of this problem could have been solved using the
    solution for the first half, but it wouldn't finish in a reasonable
    amount of time.
    
    In this case, we can optimize the solution by recognizing that the
    desired value is always in the second position of the array.  This
    allows us to just keep track of the last value that makes it into this
    index without actually having to build the list itself.

--- src/adventofcode/Day17.scala
+++ src/adventofcode/Day17.scala
@@ -18,6 +18,19 @@ object Day17 {
     list
   }
 
+  def optimizedShortCircuit(steps:Int)(iterations:Int) = {
+    var lastShortCircuit = 0
+    var position = 0
+    for (i <- 1 to iterations) {
+      val size = i
+      position = (position + steps) % size + 1
+      if (position == 1) {
+        lastShortCircuit = i
+      }
+    }
+    lastShortCircuit
+  }
+
   def main(args: Array[String]): Unit = {
     assert(buildBuffer(3)(9).toString.equals("[0, 9, 5, 7, 2, 4, 3, 8, 6, 1]"))
     val testBuffer = buildBuffer(3)(2017)
@@ -25,5 +38,8 @@ object Day17 {
 
     val buffer = buildBuffer(312)(2017)
     println(buffer.get(buffer.indexOf(2017)+1))  // 772
+
+    assert(optimizedShortCircuit(3)(9) == 9)
+    println(optimizedShortCircuit(312)(50000000))  // 42729050
   }
 }
